38c4815d86aeda709760060eca85be46118756e7
lbgc <104433318+softlberton@users.noreply.github.com>
2024-12-27 12:14:29 +0100

    Update install_vs.ps1

--- install_vs.ps1
+++ install_vs.ps1
@@ -1,1 +1,1 @@
-winget install --id Microsoft.VisualStudio.2022.Community --override "--passive --config C:\my.vsconfig"
+winget install --id Microsoft.VisualStudio.2022.Community --override "--passive --config C:\my.vsconfig" --accept-source-agreements --accept-package-agreements
